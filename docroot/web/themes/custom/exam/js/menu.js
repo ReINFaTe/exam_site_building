10fba4ebf54e02dc50632dc1f568165d468956cd
ReINFaTe <reinfate@gmail.com>
2021-08-05 15:07:22 +0300

    final

--- docroot/web/themes/custom/exam/js/menu.js
+++ docroot/web/themes/custom/exam/js/menu.js
@@ -1,17 +1,32 @@
 (function ($, Drupal) {
 Drupal.behaviors.myBehavior = {
     attach: function (context, settings) {
-      $('.region-primary-menu', context).once('mySecondBehavior').each(function () {
+      $('.region-primary-menu', context).once('myFirstBehavior').each(function () {
         let prevPos = 0;
         $(window).scroll(function(){
             $(".region-primary-menu").css("top",Math.max(0,40-$(this).scrollTop()));
             if (500-$(this).scrollTop() < 0) {
-                if (prevPos < $(this).scrollTop()) {
+                if (prevPos < $(this).scrollTop() && !$('.menu-button').hasClass('active')) {
                     $(".region-primary-menu").css("top", -100)
+                    if($(window).width() < 1200) {
+                      $(".region-primary-menu").css("top", -200)
+                    }
                 }
             }
             prevPos = $(this).scrollTop();
         });
       });
     }
-  }})(jQuery, Drupal);
+  }
+  Drupal.behaviors.mySecondBehavior = {
+    attach: function (context, settings) {
+      $('.menu-button').once("beh").click(function() {
+        $('.menu-button').toggleClass('active')
+        $('.menu--main').toggleClass('active')
+        $('.search-block-form').toggleClass('active')
+        $('.field--name-field-social-').toggleClass('active')
+      })
+    }
+  }
+}
+  )(jQuery, Drupal);
